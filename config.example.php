41fb6ad76813db7a4130a66c373c3218aec9e586
PHPeter <phpeter@phpeter.fr>
2019-11-19 17:18:31 +0100

    Add config file & ignore it

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * !!! IMPORTANT !!!
+ *
+ * Rename this file config.dist.php
+ */
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'justauthme';
+const DB_USER = 'root';
+const DB_PASS = 'root';
--- customer.php
+++ customer.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
+require_once 'config.example.php';
 
 abstract class DB {
     private static
