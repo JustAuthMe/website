c98c9bdee2cff2257886ee767cab2ae29de0fdbe
PHPeter <phpeter@phpeter.fr>
2020-04-14 02:15:22 +0200

    Add framework

---
+++ app/controllers/home.php
@@ -0,0 +1,3 @@
+<?php
+\PitouFW\Core\Data::get()->add('TITLE', 'Accueil');
+\PitouFW\Core\Controller::renderView('home/home');
---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 18/11/2018
+ * Time: 15:12
+ */
+
+const ROUTES = [
+    'home' => 'home',
+    'api' => []
+];
