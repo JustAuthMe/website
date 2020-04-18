e8d236b426fb7370d9da4063e935b86ac612bfa0
root <root@mail.justauth.me>
2020-04-18 15:08:49 +0200

    Add matomo tracking code + RGPD compliancy

--- app/views/mainView.php
+++ app/views/mainView.php
@@ -18,6 +18,18 @@ use PitouFW\Core\Request;
 	</head>
 
 	<body>
+        <div class="my-cookies" id="cookies_prompt">
+            <aside class="actions">
+                <button onclick="setCookiesChoice(true)" class="btn btn-primary">Accepter</button>
+                <button onclick="setCookiesChoice(false)" class="btn btn-primary ml-2">Refuser</button>
+            </aside>
+            <p>
+                Ce site utilise des cookies à des fins de statistiques de visites, en interne uniquement et grâce au
+                logiciel auto-hébergé <a href="https://fr.matomo.org/why-matomo/" target="_blank">Matomo</a>, sans aucune
+                utilisation de tracker externe. Pour nous autoriser à inclure votre visite de notre site dans nos
+                statistiques, cliquez sur "Accepter". Sinon, cliquez sur "Refuser".
+            </p>
+        </div>
         <header class="header fixed-top">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="<?= WEBROOT ?>">
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -39,4 +39,47 @@ document.getElementsByClassName('chevron-link')[0].onclick = e => {
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+const localStorageKey = 'cookieChoice';
+const cookiesChoice = localStorage.getItem(localStorageKey);
+
+function loadAnalytics() {
+    const matomo = 'var _paq = window._paq || [];' +
+        '_paq.push([\'trackPageView\']);' +
+        '_paq.push([\'enableLinkTracking\']);' +
+        '(function() {' +
+        '    var u="//analytics.justauth.me/";' +
+        '    _paq.push([\'setTrackerUrl\', u+\'matomo.php\']);' +
+        '    _paq.push([\'setSiteId\', \'1\']);' +
+        '    var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];' +
+        '    g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u+\'matomo.js\'; s.parentNode.insertBefore(g,s);' +
+        '})();';
+
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    try {
+        script.appendChild(document.createTextNode(matomo));
+        document.body.appendChild(script);
+    } catch (e) {
+        script.text = matomo;
+        document.body.appendChild(script);
+    }
+}
+
+function setCookiesChoice(choice) {
+    if (choice) {
+        loadAnalytics();
+    }
+
+    localStorage.setItem(localStorageKey, choice ? 'ok' : 'ko');
+    document.getElementById('cookies_prompt').style.display = 'none';
+}
+
+if (cookiesChoice === 'ok' || cookiesChoice === 'ko') {
+    document.getElementById('cookies_prompt').style.display = 'none';
+}
+
+if (cookiesChoice === 'ok') {
+    loadAnalytics();
+}
